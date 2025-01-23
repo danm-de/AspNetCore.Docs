fe2dab29cf84d3b00934ab3f8aed57a99350eadd
Daniel Mueller <github@danm.de>
2025-01-23 10:40:12 +0100

    Remove an erroneous code comment.
    PAR (Pushed Authorization Request) is not available in .NET versions prior 9. However, since these are the release notes of .NET9, the code example works correctly.

--- aspnetcore/release-notes/aspnetcore-9/samples/PAR/Program.cs
+++ aspnetcore/release-notes/aspnetcore-9/samples/PAR/Program.cs
@@ -16,10 +16,6 @@ var builder = WebApplication.CreateBuilder(args);
          // Other provider-specific configuration goes here.
  
          // The default value is PushedAuthorizationBehavior.UseIfAvailable.
- 
-         // 'OpenIdConnectOptions' does not contain a definition for 'PushedAuthorizationBehavior'
-         // and no accessible extension method 'PushedAuthorizationBehavior' accepting a first argument
-         // of type 'OpenIdConnectOptions' could be found
          oidcOptions.PushedAuthorizationBehavior = PushedAuthorizationBehavior.Disable;
      });
 // </snippet_1>
